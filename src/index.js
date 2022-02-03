29ef54c877241194c664458007d700a150c77aba
IvanTurak <tippit1988@gmail.com>
2022-02-03 10:22:08 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const { stringify } = require("mocha/lib/utils");
+
 module.exports = function reverse (n) {
-  
+    let nStr = n.toString();
+    let result = '';
+    let nStr1 = '';
+        if (nStr.startsWith('-')) {
+           nStr1 += nStr.substring(1);
+           result += nStr1.split("").reverse().join("");
+            return Number(result);
+        } else {
+            result += nStr.split("").reverse().join("");
+            return Number(result);
+        }
 }
+
+
+
+
